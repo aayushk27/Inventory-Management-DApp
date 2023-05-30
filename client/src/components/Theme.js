8ce6d4e6d15e4e7076e10139b077e327faaa092e
aayushk27 <lit2020012@iiitl.ac.in>
2023-05-30 20:16:00 +0530

    first commit

---
+++ client/src/components/Theme.js
@@ -0,0 +1,16 @@
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
+
+const theme = createMuiTheme({
+    palette: {
+      primary: {
+        // main: "#1a237e",
+        main: "#6096B4",
+      },
+      secondary: {
+        main: "#DFA67B",
+      },
+
+    },
+  });
+  
+  export default theme;
